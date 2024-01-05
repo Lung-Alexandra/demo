872ac2c8a1ff1127194074e8550f9aeab80bdbf0
Katherine456719 <alexandra.lung@s.unibuc.ro>
2024-01-05 12:09:28 +0200

    Add cv

--- js/base.js
+++ js/base.js
@@ -1,21 +1,24 @@
+// import {renderCv} from "./index.js";
+
 $.get("navbar.html", function (data) {
     $("nav").replaceWith(data);
 });
 
-$.ajax({
-    url: "content.txt",
-    dataType: "text",
-    success: function (data) {
-        resultArray = data.split("Section").filter(function (value) {
-            return value !== "";
-        });
-
-        resultArray = resultArray.map(function (element) {
-            return element.split("\r\n").filter(function (value) {
-                return value.trim() !== ""; // Remove empty strings after splitting
-            });
-        });
-    }
-});
-
-
+// $.ajax({
+//     url: "content.txt",
+//     dataType: "text",
+//     success: function (data) {
+//         let resultArray = data.split("Section").filter(function (value) {
+//             return value !== "";
+//         });
+//
+//         resultArray = resultArray.map(function (element) {
+//             return element.split("\r\n").filter(function (value) {
+//                 return value.trim() !== ""; // Remove empty strings after splitting
+//             });
+//         });
+//
+//         // renderCv(resultArray);
+//         console.log(resultArray);
+//     }
+// });
--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
-//
-// // Find the subarray starting with "Personal information" at the first position
-//
-// var personalInformationSubarray = resultArray.find(function (subarray) {
-//     return subarray[0] === "Personal information";
-// });
-// personalInformationSubarray.forEach(function (content, index) {
-//     if (index !== 0) {
-//         // Create a new ul element
-//         $(".page-section-content").append("<p>" + content + "</p>");
-//     }
-//     else{
-//         $(".page-section-title").append(content);
-//     }
-// });
+
+function renderCv(cvArray) {
+    // Find the subarray starting with "Personal information" at the first position
+    var personalInformationSubarray = cvArray.find(function (subarray) {
+        return subarray[0] === "Personal information";
+    });
+    personalInformationSubarray.forEach(function (content, index) {
+        if (index !== 0) {
+            // Create a new ul element
+            $(".page-section-content").append("<p>" + content + "</p>");
+        } else {
+            $(".page-section-title").append(content);
+        }
+    });
+}
+
+export {renderCv}
