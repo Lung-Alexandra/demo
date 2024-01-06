97a38452ff1ad732044edba0cf2f462b2fdd1e3f
Katherine456719 <alexandra.lung@s.unibuc.ro>
2024-01-07 00:05:18 +0200

    Update base.css, cv.css, and 4 more files...

--- js/cv.js
+++ js/cv.js
@@ -1,5 +1,56 @@
-$(function() {
-    $('.page-section-content').eq(3).css('align-items', 'initial');
-   // x.attr("alignItems","initial");
-});
+$.getJSON('cv.json', function (jsonData) {
+// Function to generate HTML content for a section
+    function generateSectionHTML(title, content) {
+        var sectionHTML = '<div class="page-section">' +
+            '<div class="page-section-title">' + title + '</div>' +
+            '<div class="line"></div>' +
+            '<div class="geometric-box"></div>' +
+            '<div class="page-section-content">';
 
+        let nrPhrases = 0;
+        $.each(content, function (index, item) {
+            if (item.hasOwnProperty("label")) {
+                sectionHTML += '<p><span class="bold">' + item.label + ':</span> ' + item.value + '</p>';
+            } else if (item.hasOwnProperty("year")) {
+                sectionHTML += '<div class="job">' +
+                    '<div class="year">' + item.year + '</div>' +
+                    '<div class="where">' + item.where ;
+
+                if (item.hasOwnProperty("thesisTitle")) {
+                    var advisors = Array.isArray(item.advisors) ? item.advisors.join(', ') : item.advisors
+                    sectionHTML += '<p>Title of thesis: ' + item.thesisTitle + '<br>Advisor: ' + advisors + '</p>';
+                }
+
+                sectionHTML += '</div></div>';
+            } else if (item.hasOwnProperty("phrase")) {
+                sectionHTML += '<p>' + item.phrase + '</p>';
+                nrPhrases +=1;
+            } else if (item.hasOwnProperty("title")) {
+                sectionHTML += '<div class="project">';
+
+                if (item.hasOwnProperty("link"))
+                    sectionHTML += '<div class="title"> <span class="bold">' + (index - nrPhrases + 1) + '. </span><a href="' + item.link + '" class="link">' + item.title + '</a></div>'
+                else sectionHTML += '<div class="title"><span class="bold">' + (index - nrPhrases + 1) + '. </span>' + item.title + '</div>';
+                var team = Array.isArray(item.team) ? item.team.join(', ') : item.team
+
+                sectionHTML += '<p>' + team + '</p>';
+                sectionHTML += '</div>';
+            } else if (item.hasOwnProperty("activity")) {
+                sectionHTML += '<p>' + item.activity + '<a href="' + item.link + '" class="link">' + item.linkwords + '</a></p>';
+            }
+        });
+
+
+        return sectionHTML;
+    }
+
+    // Display data for each section
+    jsonData.sections.forEach(function (section, index) {
+        $(".content").append(generateSectionHTML(section.title, section.content));
+        $.each(section.style, function (property, value) {
+            $('.page-section-content').eq(index).css(property, value);
+        });
+
+    });
+
+});
--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 
-function renderCv(cvArray) {
-    // Find the subarray starting with "Personal information" at the first position
-    var personalInformationSubarray = cvArray.find(function (subarray) {
-        return subarray[0] === "Personal information";
-    });
-    personalInformationSubarray.forEach(function (content, index) {
-        if (index !== 0) {
-            // Create a new ul element
-            $(".page-section-content").append("<p>" + content + "</p>");
-        } else {
-            $(".page-section-title").append(content);
-        }
-    });
-}
-
-export {renderCv}
+// function renderCv(cvArray) {
+//     // Find the subarray starting with "Personal information" at the first position
+//     var personalInformationSubarray = cvArray.find(function (subarray) {
+//         return subarray[0] === "Personal information";
+//     });
+//     personalInformationSubarray.forEach(function (content, index) {
+//         if (index !== 0) {
+//             // Create a new ul element
+//             $(".page-section-content").append("<p>" + content + "</p>");
+//         } else {
+//             $(".page-section-title").append(content);
+//         }
+//     });
+// }
+//
+// export {renderCv}
